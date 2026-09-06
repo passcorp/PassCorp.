92db2322b1a9587785d3c621bb3f76f03994c75e
passcorp <sales@passcorp.in>
2026-09-06 21:35:11 +0530

    Add PASS CORP Unified Desktop Suite (Quotor + Invotor ERP) for macOS and Windows

---
+++ downloads/PASS_CORP_Suite_Windows.bat
@@ -0,0 +1,26 @@
+@echo off
+title PASS CORP. Enterprise Suite - Quotor & Invotor
+echo Starting PASS CORP. Unified Suite...
+
+if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" (
+    start "" "%ProgramFiles%\Google\Chrome\Application\chrome.exe" --app=https://passcorp.in/suite.html
+    exit
+)
+if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" (
+    start "" "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" --app=https://passcorp.in/suite.html
+    exit
+)
+if exist "%LocalAppData%\Google\Chrome\Application\chrome.exe" (
+    start "" "%LocalAppData%\Google\Chrome\Application\chrome.exe" --app=https://passcorp.in/suite.html
+    exit
+)
+if exist "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe" (
+    start "" "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe" --app=https://passcorp.in/suite.html
+    exit
+)
+if exist "%ProgramFiles%\Microsoft\Edge\Application\msedge.exe" (
+    start "" "%ProgramFiles%\Microsoft\Edge\Application\msedge.exe" --app=https://passcorp.in/suite.html
+    exit
+)
+
+start https://passcorp.in/suite.html
